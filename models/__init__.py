4af839119ce78d24615c87f0695faf01ee5d462f
Ezzeddin Essam <ej_dozze95@outlook.com>
2023-12-09 05:12:32 -0800

    Store first object

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,9 @@
 """
 package initialization model
 """
+
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ attributes/methods for other classes
 """
 from datetime import datetime
 import uuid
+import models
 
 
 class BaseModel:
@@ -20,17 +21,20 @@ class BaseModel:
             *args: Variable length argument list.
             **kwargs: Arbitrary keyword arguments.
         """
+        dt_frmt = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                        format_obj = datetime.strptime(value, dt_frmt)
+                        setattr(self, key, format_obj)
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -43,6 +47,7 @@ class BaseModel:
         Updates the 'updated_at' attribute with the current datetime.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
